f6759759a6682d8ceb2083a0fe57f567956432b8
Lukas Volgger <lukas.volgger@live.at>
2022-10-20 08:46:09 +0200

    get data from firestore and display it in a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase-to-do-list';
+  todos: Array<any>;
+
+  todos$: Observable<any>; // todos$ is a variable that changes every time data is updated in the firestore
+  constructor(firestore: Firestore) { // Here the Firestore object is imported
+    const coll = collection(firestore, 'todos'); // Here the collection which was created in the Firestore is assigned to the constant coll
+    this.todos$ = collectionData(coll);
+
+    this.todos$.subscribe((todosFromFirestore) => {
+      console.log('New to-do\'s: ', todosFromFirestore);
+      this.todos = todosFromFirestore;
+    })
+  }
 }
