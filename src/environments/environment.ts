4a8465dc0497cfa832d97a6bdfff6acde7db9495
Lukas Volgger <lukas.volgger@live.at>
2022-10-20 07:55:08 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'lv-firebase-to-do-list',
+    appId: '1:834193570684:web:ee3eea78712b5eadea02f9',
+    storageBucket: 'lv-firebase-to-do-list.appspot.com',
+    apiKey: 'AIzaSyAbuH1FZ5L11MCH2fx12xUCgGn3NHEy3fE',
+    authDomain: 'lv-firebase-to-do-list.firebaseapp.com',
+    messagingSenderId: '834193570684',
+  },
   production: false
 };
 
