93ef63e09340f4d728ca4d9be204714d505deed8
Lukas Volgger <lukas.volgger@live.at>
2022-10-20 10:39:26 +0200

    implement addTodo method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, setDoc, doc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -10,9 +10,10 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   title = 'firebase-to-do-list';
   todos: Array<any>;
+  todoInput: string = '';
 
   todos$: Observable<any>; // todos$ is a variable that changes every time data is updated in the firestore
-  constructor(firestore: Firestore) { // Here the Firestore object is imported
+  constructor(private firestore: Firestore) { // Here the Firestore object is imported
     const coll = collection(firestore, 'todos'); // Here the collection which was created in the Firestore is assigned to the constant coll
     this.todos$ = collectionData(coll);
 
@@ -20,5 +21,11 @@ export class AppComponent {
       console.log('New to-do\'s: ', todosFromFirestore);
       this.todos = todosFromFirestore;
     })
+  }
+
+  addTodo() {
+    console.log(this.todoInput);
+    const coll = collection(this.firestore, "todos");
+    return setDoc(doc(coll), { name: this.todoInput });
   }
 }
